da024c699c5784fa52714628fcbd1185f5139482
duelingthreads <duelingthreads@gmail.com>
2018-11-21 18:40:32 -0600

    Updating ipynb and adding Flask App py file

---
+++ climateapp.py
@@ -0,0 +1,71 @@
+#Import Libraries
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+import datetime as dt
+from flask import Flask, jsonify
+
+##---------- Database Setup ----------##
+#create engine
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables from engine
+Base.prepare(engine, reflect=True)
+# We can view all of the classes that automap found
+Base.classes.keys()
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+##---------- Flask Setup ---------##
+app = Flask(__name__)
+
+#Flask Routes
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitations():
+    #Query Precipitation - All Data in the last year of data
+    precipitation =session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= '2016-08-23').all()
+    '''Convert the query results to a dictionary using date as the key and prcp as the value. Return the JSON representation of the dict.'''
+    all_prcp = []
+    for p in precipitation:
+        prcp_dict = {}
+        prcp_dict["date"] = p.date
+        prcp_dict["prcp"] = p.prcp
+        all_prcp.append(prcp_dict)
+    return jsonify(all_prcp)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Query the stations and the counts in descending order.
+    station_activity = session.query(Measurement.station,func.count(Measurement.station).label('count')).\
+    group_by(Measurement.station).\
+    order_by(func.count(Measurement.station).desc()).all()
+    all_stat = []
+    for s in station_activity:
+        stat_dict = {}
+        stat_dict["Station ID"] = s.station
+        stat_dict["Num Meas"] = s.count
+        all_stat.append(stat_dict)
+    return jsonify(all_stat)
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
